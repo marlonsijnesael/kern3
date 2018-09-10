1fc7024fd3bb3dcb81cc9bf822d16a5c51f7a383
marlon sijnesael <fakiefsflip@gmail.com>
2018-09-10 23:03:26 +0200

    prefabs

---
+++ Assets/Scripts/Rooms.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Rooms {
+
+    public Vector2 gridPos;
+
+    public int type;
+
+    public bool doorTop, doorBot, doorLeft, doorRight;
+
+    public Rooms(Vector2 _gridPos, int _type) {
+        gridPos = _gridPos;
+        type = _type;
+        }
+
+    }
+
+
