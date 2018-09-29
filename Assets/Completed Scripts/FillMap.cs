173ccde722d9937ba5f1672626a828aa1800c2f0
marlon sijnesael <fakiefsflip@gmail.com>
2018-09-29 23:50:21 +0200

    reworked procedural generation

---
+++ Assets/Completed Scripts/FillMap.cs
@@ -0,0 +1,236 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// this class is used to fill the created 2d node grid with rooms and walls
+/// </summary>
+public class FillMap : MonoBehaviour {
+
+    private GridGenerator _gridReference;
+    private List<RoomNode> path = new List<RoomNode>();
+
+    [Header("Iterations")]
+    public int amountOfIterations;
+
+    [Header("Prefabs")]
+    public GameObject walkWay;
+    public GameObject wall;
+    public GameObject roomPrefab;
+
+    private void Start() {
+        _gridReference = GetComponent<GridGenerator>();
+        CreateRooms();
+        }
+
+    //Creates a path between two random nodes and repeats this proces for the amount of iterations wanted. 
+    //when This is done, it determines if the node is a Room or a wall and Instantiates the right prefab;
+    public void CreateRooms() {
+
+        Vector2Int room1 = new Vector2Int(Random.Range(1, _gridReference.arraySizeX - 1), Random.Range(1, _gridReference.arraySizeY - 1));
+        Vector2Int room2 = new Vector2Int(Random.Range(1, _gridReference.arraySizeX - 1), Random.Range(1, _gridReference.arraySizeY - 1));
+        RoomNode node1 = _gridReference.roomNodeArray[room1.x, room1.y];
+        RoomNode node2 = _gridReference.roomNodeArray[room2.x, room2.y];
+
+        for (int i = 0; i < amountOfIterations; i++) {
+            FindPath(node1, node2);
+            node1 = node2;
+            room1 = new Vector2Int(Random.Range(1, _gridReference.arraySizeX - 1), Random.Range(1, _gridReference.arraySizeY - 1));
+            node2 = _gridReference.roomNodeArray[room1.x, room1.y];
+            }
+
+        foreach (RoomNode room in _gridReference.roomNodeArray) {
+            if (room.type != 1) {
+                room.type = 0;
+                }
+            if (room.type == 1) {
+                InstantiateNode(room, roomPrefab, true);
+                SetDoors(room);
+                } else if (room.type == 0) {
+                InstantiateNode(room, wall, false);
+                }
+            }
+        }
+
+    //function to instantiate the nodes without code repitition
+    public void InstantiateNode(RoomNode _room, GameObject _prefab, bool _filled) {
+        _room.self = Instantiate(_prefab);
+        _room.isFilled = _filled;
+        _room.InitSelf();
+        }
+
+    //reverses the path produced by the a* pathfinding and marks all nodes along the path with the type 1 (room)
+    public void Trace(RoomNode _startNode, RoomNode _endNode) {
+        path = new List<RoomNode>();
+        RoomNode currentNode = _endNode;
+
+        while (currentNode != _startNode) {
+            path.Add(currentNode);
+            currentNode = currentNode.parent;
+            currentNode.type = 1;
+            }
+        path.Reverse();
+        }
+
+    //A* pathfinding algoritm from tutorial:
+    public void FindPath(RoomNode startNode, RoomNode targetNode) {
+        List<RoomNode> openSet = new List<RoomNode>();
+        HashSet<RoomNode> closedSet = new HashSet<RoomNode>();
+        openSet.Add(startNode);
+
+        while (openSet.Count > 0) {
+            RoomNode node = openSet[0];
+            for (int i = 1; i < openSet.Count; i++) {
+                if (openSet[i].fCost < node.fCost || openSet[i].fCost == node.fCost) {
+                    if (openSet[i].hCost < node.hCost)
+                        node = openSet[i];
+                    }
+                }
+
+            openSet.Remove(node);
+            closedSet.Add(node);
+
+            if (node == targetNode) {
+                Trace(startNode, targetNode);
+
+                startNode.ChangeColor(Color.red);
+                targetNode.ChangeColor(Color.blue);
+                Debug.Log("found");
+                return;
+                }
+
+            foreach (RoomNode neighbour in _gridReference.LoopThrough(node)) {
+                if (closedSet.Contains(neighbour)) {
+                    continue;
+                    }
+
+                int newCostToNeighbour = node.gCost + _gridReference.GetDistance(node, neighbour);
+                if (newCostToNeighbour < neighbour.gCost || !openSet.Contains(neighbour)) {
+                    neighbour.gCost = newCostToNeighbour;
+                    neighbour.hCost = _gridReference.GetDistance(neighbour, targetNode);
+                    neighbour.parent = node;
+
+                    if (!openSet.Contains(neighbour))
+                        openSet.Add(neighbour);
+                    }
+                }
+
+            }
+        }
+
+    //checks if room needs doors/walls
+    public void SetDoors(RoomNode _node) {
+        Doors doorScript = _node.self.GetComponent<Doors>();
+        for (int x = -1; x <= 1; x++) {
+            for (int y = -1; y <= 1; y++) {
+                //check all nodes left, right, up and down from the current node
+                if (x == 1 && y == 0 || x == 0 && y == 1 || x == -1 && y == 0 || x == 0 && y == -1) {
+
+                    int checkX = _node.gridX + x;
+                    int checkY = _node.gridY + y;
+
+                    //check if coordinates are actually on grid
+                    if (checkX >= 0 && checkX < _gridReference.arraySizeX && checkY >= 0 && checkY < _gridReference.arraySizeY) {
+                        RoomNode NeighbournNode = _gridReference.roomNodeArray[checkX, checkY];
+
+                        //check types of surrounding nodes and determine if there should be a wall
+
+                        if (NeighbournNode.type != 0) {
+
+                            if (x == 1 && y == 0) {
+                                doorScript.hasDoorEast = true;
+                                }
+                            if (x == -1 && y == 0) {
+                                doorScript.hasDoorWest = true;
+                                }
+                            if (x == 0 && y == 1) {
+                                doorScript.hasDoorNorth = true;
+                                }
+                            if (x == 0 && y == -1) {
+                                doorScript.hasDoorSouth = true;
+                                }
+
+                            }
+                        doorScript.SetDoors();
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+/*
+public void CountParents(RoomNode startNode, RoomNode endNode) {
+    RoomNode currentNode = endNode;
+
+    while (currentNode != startNode) {
+        startNode.parentCount++;
+        currentNode = currentNode.parent;
+
+        }
+    }
+
+void RetracePath(RoomNode startNode, RoomNode endNode) {
+    List<RoomNode> path = new List<RoomNode>();
+    RoomNode currentNode = endNode;
+
+    while (currentNode != startNode) {
+        path.Add(currentNode);
+        currentNode = currentNode.parent;
+
+        //currentNode.ChangeColor(Color.yellow);
+        }
+    path.Reverse();
+    foreach (RoomNode node in path) {
+        if (!_gridInstance.filledRooms.Contains(node)) {
+            CreatePath(node);
+            _gridInstance.filledRooms.Add(node);
+            node.type = 2;
+            //_gridInstance.filledRooms.Remove(startNode);
+            _gridInstance.FindNeighbours();
+
+
+            }
+
+
+        //nestedRoomList.Add(path);
+
+
+        }
+    }
+
+public void CreatePath(RoomNode tile) {
+    // foreach (RoomNode tile in _path) {
+
+    GameObject floorTile = Instantiate(walkWay);
+    Vector3 worldPoint = _gridInstance.worldBottomLeft + Vector3.right * (tile.gridX * _gridInstance.nodeDiameter + _gridInstance.nodeRadius) + Vector3.forward * (tile.gridY * _gridInstance.nodeDiameter + _gridInstance.nodeRadius);
+    walkWay.transform.position = worldPoint;
+
+    //floorTile.GetComponent<MeshRenderer>().material.color = Color.yellow;
+
+    }
+
+public void ClosestRoom(RoomNode _nodeA) {
+    List<RoomNode> filledRooms = _gridInstance.filledRooms;
+    int lowest = 100;
+    RoomNode tempRoom = _nodeA;
+
+    foreach (RoomNode room in filledRooms) {
+        if (room == _nodeA || room == prevRoom || room.type == 2)
+            continue;
+        int dist = _gridInstance.GetDistance(_nodeA, room);
+        if (dist < lowest && dist > 10) {
+            tempRoom = room;
+            lowest = dist;
+            }
+        // Debug.Log(nestedRoomList.Count);
+        }
+
+    FindPath(_nodeA, tempRoom);
+    prevRoom = _nodeA;
+
+    //CreatePath(finalPath);
+    // Debug.Log(nestedRoomList.Count);
+    }
+    */
---
+++ Assets/Old or not working/FindPath.cs
@@ -0,0 +1,223 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+/*
+public class FindPath : MonoBehaviour {
+
+    private GridGenerator _gridInstance;
+    List<RoomNode> path = new List<RoomNode>();
+    List<List<RoomNode>> nestedRoomList = new List<List<RoomNode>>();
+    List<RoomNode> potentialPaths = new List<RoomNode>();
+    public GameObject walkWay;
+    public GameObject wall;
+
+    //public RoomNode tempNode;
+    public RoomNode prevRoom;
+    public int tempShortest = 0;
+    private void Start() {
+        _gridInstance = GetComponent<GridGenerator>();
+        for (int i = 0; i < _gridInstance.filledRooms.Count; i++) {
+            ClosestRoom(_gridInstance.filledRooms[i]);
+        }
+
+        foreach (RoomNode node in _gridInstance.roomNodeArray) {
+            if (node.gridX == 0 || node.gridX == _gridInstance.arraySizeX -1 || node.gridY == 0 || node.gridY == _gridInstance.arraySizeY -1) {
+                node.type = 0;
+                }
+            if (node.type ==0) {
+                GameObject wallTile = Instantiate(wall);
+                Vector3 worldPoint = _gridInstance.worldBottomLeft + Vector3.right * (node.gridX * _gridInstance.nodeDiameter + _gridInstance.nodeRadius) + Vector3.forward * (node.gridY * _gridInstance.nodeDiameter + _gridInstance.nodeRadius);
+                wallTile.transform.position = worldPoint;
+                wallTile.name = "wall";
+            }
+        }
+
+        foreach (RoomNode room in _gridInstance.filledRooms) {
+            SetDoors(room);
+            }
+
+    }
+    
+    public void CreateWalls() {
+
+        }
+
+    public void Update() {
+        
+        if (Input.GetKeyDown(KeyCode.X)) {
+          
+            tempShortest++;
+        }        
+
+    }
+
+    void CountParents(RoomNode startNode, RoomNode endNode) {
+        RoomNode currentNode = endNode;
+       
+        while (currentNode != startNode) {
+            startNode.parentCount++;
+            currentNode = currentNode.parent;
+            
+            }
+        }
+
+    void RetracePath(RoomNode startNode, RoomNode endNode) {
+        List<RoomNode> path = new List<RoomNode>();
+        RoomNode currentNode = endNode;
+
+        while (currentNode != startNode) {
+            path.Add(currentNode);
+            currentNode = currentNode.parent;
+
+            //currentNode.ChangeColor(Color.yellow);
+        }
+        path.Reverse();
+        foreach (RoomNode node in path) {
+            if (!_gridInstance.filledRooms.Contains(node)) {
+                CreatePath(node);
+                _gridInstance.filledRooms.Add(node);
+                node.type = 2;
+                //_gridInstance.filledRooms.Remove(startNode);
+                _gridInstance.FindNeighbours();
+
+
+            }
+
+
+            //nestedRoomList.Add(path);
+
+
+        }
+    }
+
+    public void CreatePath(RoomNode tile) {
+       // foreach (RoomNode tile in _path) {
+       
+            GameObject floorTile = Instantiate(walkWay);
+        Vector3 worldPoint = _gridInstance.worldBottomLeft + Vector3.right * (tile.gridX * _gridInstance.nodeDiameter + _gridInstance.nodeRadius) + Vector3.forward * (tile.gridY * _gridInstance.nodeDiameter + _gridInstance.nodeRadius);
+        walkWay.transform.position = worldPoint ;
+      
+        //floorTile.GetComponent<MeshRenderer>().material.color = Color.yellow;
+
+    }
+
+    public void ClosestRoom(RoomNode _nodeA) {
+        List<RoomNode> filledRooms = _gridInstance.filledRooms;
+        int lowest = 100;
+        RoomNode tempRoom = _nodeA;
+
+        foreach (RoomNode room in filledRooms) {
+            if (room == _nodeA || room == prevRoom || room.type ==2)
+               continue;
+            int dist = _gridInstance.GetDistance(_nodeA, room);
+            if (dist < lowest && dist > 10) {
+                tempRoom = room;
+                lowest = dist;
+            }
+           // Debug.Log(nestedRoomList.Count);
+        }
+
+        Path(_nodeA, tempRoom);
+        prevRoom = _nodeA;
+
+        //CreatePath(finalPath);
+        // Debug.Log(nestedRoomList.Count);
+    }
+
+    
+
+
+    void  Path(RoomNode startNode, RoomNode targetNode) {
+       // Vector2Int gridposStart = _gridInstance.startNode;
+        //vector2Int gridposTarget = _gridInstance.endNode;
+        //RoomNode startNode = _gridInstance.roomNodeArray[gridposStart.x, gridposStart.y];
+        //RoomNode targetNode = _gridInstance.roomNodeArray[gridposTarget.x, gridposTarget.y];
+
+        List<RoomNode> openSet = new List<RoomNode>();
+        HashSet<RoomNode> closedSet = new HashSet<RoomNode>();
+        
+        openSet.Add(startNode);
+        
+
+        while (openSet.Count > 0) {
+            RoomNode node = openSet[0];
+            for (int i = 1; i < openSet.Count; i++) {
+                if (openSet[i].fCost < node.fCost || openSet[i].fCost == node.fCost) {
+                    if (openSet[i].hCost < node.hCost)
+                        node = openSet[i];
+                }
+            }
+
+            openSet.Remove(node);
+            closedSet.Add(node);
+
+            if (node == targetNode) {
+                 RetracePath(startNode, targetNode);
+                
+                startNode.ChangeColor(Color.red);
+                targetNode.ChangeColor(Color.blue);
+                Debug.Log("found");
+                return ;
+            }
+
+            foreach (RoomNode neighbour in _gridInstance.LoopThrough(node)) {
+                if ( closedSet.Contains(neighbour)) {
+                    continue;
+                }
+
+                int newCostToNeighbour = node.gCost + _gridInstance.GetDistance(node, neighbour);
+                if (newCostToNeighbour < neighbour.gCost || !openSet.Contains(neighbour)) {
+                    neighbour.gCost = newCostToNeighbour;
+                    neighbour.hCost = _gridInstance.GetDistance(neighbour, targetNode);
+                    neighbour.parent = node;
+
+                    if (!openSet.Contains(neighbour))
+                        openSet.Add(neighbour);
+                }
+            }
+         
+        }
+    }
+
+
+    void SetDoors(RoomNode _inTarray) {
+        List<RoomNode> neighbours = new List<RoomNode>();
+        Doors door = _inTarray.self.GetComponent<Doors>();
+        for (int x = -1; x <= 1; x++) {
+            for (int y = -1; y <= 1; y++) {
+                if (x == 1 && y == 0 || x == 0 && y == 1 || x == -1 && y == 0 || x == 0 && y == -1) {
+                    int checkX = _inTarray.gridX + x;
+                    int checkY = _inTarray.gridY + y;
+                    if (checkX >= 0 && checkX < _gridInstance.arraySizeX && checkY >= 0 && checkY < _gridInstance.arraySizeY) {
+                        RoomNode node = _gridInstance.roomNodeArray[checkX, checkY];
+
+
+                        
+
+                        if (node.type != 0) {
+
+                            if (x == 1 && y == 0) {
+                                door.hasDoorEast = true;
+                                }
+                            if (x == -1 && y == 0) {
+                                door.hasDoorWest = true;
+                                }
+                            if (x == 0 && y == 1) {
+                                door.hasDoorNorth = true;
+                                }
+                            if (x == 0 && y == -1) {
+                                door.hasDoorSouth = true;
+                                }
+
+                            }
+
+                       
+                        }
+                    }
+                }
+            }
+        door.SetDoors();
+        }
+    }
+    */
